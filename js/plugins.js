032a6d3cb901f4096e144944090b8396624aae57
GlistValera <den_rzaev@mail.ru>
2020-08-09 22:23:01 +0300

    нужна помощь с секцией team (acco)

--- js/plugins.js
+++ js/plugins.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
     $('.slick__list').bxSlider();
   });
+
+
+  // team acco
+
+$(function() {
+  $('.team-trigger').on('click', e => {
+    e.preventDefault()
+
+    const  $this= $(e.currentTarget);
+    const item = $this.closest('.team__item');
+    const content = item.find('.team-trigger__content');
+    
+    item.toggleClass('team__item_active');
+    content.slideToggle(1000);
+
+  })
+})
