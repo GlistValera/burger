4f7ca9c625dd582550ed896e54b42ada1b907adb
GlistValera <den_rzaev@mail.ru>
2020-08-12 02:13:51 +0300

    сделал аккордеон в секции team, не успел сделать выпадашку. Спросить про one page scroll

--- js/plugins.js
+++ js/plugins.js
@@ -1,20 +1,70 @@
-$(document).ready(function(){
-    $('.slick__list').bxSlider();
-  });
+$(document).ready(function () {
+  $('.slick__list').bxSlider();
+});
 
 
-  // team acco
+// team acco
 
-$(function() {
+$(function () {
   $('.team-trigger').on('click', e => {
     e.preventDefault()
 
-    const  $this= $(e.currentTarget);
+    const $this = $(e.currentTarget);
+    const container = $this.closest('.team__list');
     const item = $this.closest('.team__item');
-    const content = item.find('.team-trigger__content');
-    
-    item.toggleClass('team__item_active');
-    content.slideToggle(500);
+    const items = $('.team__item', container);
+    const content = $('.team-trigger__content', item);
+    const otherContent = $('.team-trigger__content', container);
+    const textBlock = $('.team-trigger__align', item);
+    const reqHeight = textBlock.outerHeight();
 
+    if (!item.hasClass('team__item_active')) {
+      items.removeClass('team__item_active')
+      item.addClass('team__item_active')
+
+      otherContent.css({
+        'height': 0
+      })
+
+      content.css({
+        'height': reqHeight
+      })
+
+    } else {
+      item.removeClass('team__item_active');
+      content.css({
+        'height': 0
+      })
+    }
+    // item.toggleClass('team__item_active');
+    // content.slideToggle(500);
   })
 })
+
+
+const display = $('.maincontent');
+const sections = $('.section');
+
+const kekw = sectionEq => {
+  const position = (sectionEq * -100) + '%';
+
+  display.css({
+    'transform' : 'translate(0, ${position})',
+    '-webkit-transform' : 'translate(0, ${position})'
+
+  })
+}
+
+$('.wrapper').on('wheel', e => {
+  const deltaY = e.originalEvent.deltaY;
+
+  if (deltaY > 0) {
+    // console.log('scroll down')
+    kekw(4)
+
+  }
+
+  if (deltaY < 0) {
+    // console.log('scroll up')
+  }
+});
