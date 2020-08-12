7c85b6f1559c3de5196dc78a5c1493b6dc57ab06
GlistValera <den_rzaev@mail.ru>
2020-08-13 01:34:24 +0300

    сделал аккордеон в секции меню

--- js/plugins.js
+++ js/plugins.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 });
 
 
-// team acco
+// team accordion
 
 $(function () {
   $('.team-trigger').on('click', e => {
@@ -41,39 +41,87 @@ $(function () {
   })
 })
 
+// // one page scroll
 
-const display = $('.maincontent');
-const sections = $('.section');
+// const display = $('.maincontent');
+// const sections = $('.section');
 
-const kekw = sectionEq => {
-  const position = (sectionEq * -100) + '%';
+// const kekw = sectionEq => {
+//   const position = (sectionEq * -100) + '%';
 
-  display.css({
-    'transform' : 'translate(0, ${position})',
-    '-webkit-transform' : 'translate(0, ${position})'
+//   display.css({
+//     'transform' : 'translate(0, ${position})',
+//     '-webkit-transform' : 'translate(0, ${position})'
 
-  })
-}
+//   })
+// }
 
-$('.wrapper').on('wheel', e => {
-  const deltaY = e.originalEvent.deltaY;
+// $('.wrapper').on('wheel', e => {
+//   const deltaY = e.originalEvent.deltaY;
 
-  if (deltaY > 0) {
-    // console.log('scroll down')
-    kekw(4)
+//   if (deltaY > 0) {
+//     // console.log('scroll down')
+//     kekw(4)
 
-  }
+//   }
 
-  if (deltaY < 0) {
-    // console.log('scroll up')
-  }
+//   if (deltaY < 0) {
+//     // console.log('scroll up')
+//   }
+// });
+
+$(function () {
+
+  //обрабатываем клик по блоку с классом trigger
+  $('.trigger').on('click', function (e) {
+    e.preventDefault();
+    //получаем нужные нам данные
+     $this = $(this),
+      //получаем всё блоки menu
+      container = $this.closest('.menu__bar-list'),
+      //получаем li по которому кликнули
+      item = $this.closest('.menu__bar-item'),
+      //получаем все другие li
+      items = container.find('.menu__bar-item'),
+      //выбираем из li активный
+      activeItem = items.filter('.menu__bar-item_active'),
+      //выбираем из li по которому кликнули блок контен
+      content = item.find('.trigger__content-wrap'),
+      //выбираем у li с классом active блок контент
+      activeContent = activeItem.find('.trigger__content-wrap');
+
+    //если нет li с классом active по которому кликнули
+    if (!item.hasClass('menu__bar-item_active')) {
+      //убираем класс active
+      items.removeClass('menu__bar-item_active');
+      //добавляем active кликнутому
+      item.addClass('menu__bar-item_active');
+      //у того у кого был active задаём ширину 0
+      activeContent.css({
+        'width': '0px'
+      });
+      //кликнутому 530       
+      content.css({
+        'width': '100%'
+      });
+      //иначе
+    } else {
+      item.removeClass('menu__bar-item_active');
+      content.css({
+        'width': '0px'
+      });
+
+    }
+  });
+
+  // клик вне аккордеона
+  $(document).on('click', function (e) {
+    var $this = $(e.target);
+    if (!$this.closest('.menu__bar-list').length) {
+      $('.trigger__content-wrap').css({
+        'width': '0px'
+      });
+      $('.menu__bar-item').removeClass('menu__bar-item_active');
+    }
+  });
 });
-
-
-//fancybox
-
-$('[data-fancybox]').fancybox({
-	// Options will go here
-});
-
-$(".close-modal ").addClass("blackZone")
